d17ba4545812bfccf968622f19ebad28ff203569
RuslanSMP <159168119+RuslanSMP@users.noreply.github.com>
2024-02-07 23:28:36 +0400

    Update

--- main.py
+++ main.py
@@ -1,9 +1,59 @@
 from PyQt5 import uic
+import io
 from sys import argv, exit
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from random import choice, randint
+
+
+class MyTemplate():
+    def __init__(self):
+        self.template = """<?xml version="1.0" encoding="UTF-8"?>
+        <ui version="4.0">
+         <class>MainWindow</class>
+         <widget class="QMainWindow" name="MainWindow">
+          <property name="geometry">
+           <rect>
+            <x>0</x>
+            <y>0</y>
+            <width>598</width>
+            <height>353</height>
+           </rect>
+          </property>
+          <property name="windowTitle">
+           <string>MainWindow</string>
+          </property>
+          <widget class="QWidget" name="centralwidget">
+           <widget class="QPushButton" name="Button">
+            <property name="geometry">
+             <rect>
+              <x>230</x>
+              <y>120</y>
+              <width>91</width>
+              <height>21</height>
+             </rect>
+            </property>
+            <property name="text">
+             <string>Нажми меня!</string>
+            </property>
+           </widget>
+          </widget>
+          <widget class="QMenuBar" name="menubar">
+           <property name="geometry">
+            <rect>
+             <x>0</x>
+             <y>0</y>
+             <width>598</width>
+             <height>21</height>
+            </rect>
+           </property>
+          </widget>
+          <widget class="QStatusBar" name="statusbar"/>
+         </widget>
+         <resources/>
+         <connections/>
+        </ui>"""
 
 
 class MyWidget(QMainWindow):
@@ -13,7 +63,9 @@ class MyWidget(QMainWindow):
 
     def initUI(self, Form):
         self.setGeometry(300, 300, 150, 150)
-        uic.loadUi('UI.ui', self)
+        my_ui = MyTemplate()
+        f = io.StringIO(my_ui.template)
+        uic.loadUi(f, self)
         self.do_paint = False
         self.Button.clicked.connect(self.paint)
 
@@ -37,6 +89,7 @@ class MyWidget(QMainWindow):
         leigt = randint(25, 50)
         qp.drawEllipse(x - leigt // 2, y - leigt // 2, leigt,
                        leigt)
+
 
 if __name__ == '__main__':
     app = QApplication(argv)
